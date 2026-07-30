2d0eede8bb95f4bf96021b243f52f5e93a322c1a
zaidahmedofficial <zaidahmedoffical@gmail.com>
2026-07-31 02:43:05 +0500

    NextJS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
