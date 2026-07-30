423e63540a67eb5ae7c5bf02d8e9ae7cd059eb02
zaidahmedofficial <zaidahmedoffical@gmail.com>
2026-07-31 01:59:55 +0500

    Database tur

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
 declare global {
-  // allow global `var prisma`
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
+const libsql = createClient({
+  url: process.env.DATABASE_URL!,
+});
+
 export const db =
   global.prisma ||
   new PrismaClient({
+    adapter: new PrismaLibSQL(libsql),
     log: ["query"],
   });
 
