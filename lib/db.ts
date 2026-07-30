8c4ecec18d9005d766ef0a5095b6cefc1c57e6ae
zaidahmedofficial <zaidahmedoffical@gmail.com>
2026-07-31 02:18:28 +0500

    Going On

--- lib/db.ts
+++ lib/db.ts
@@ -1,22 +1,32 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-const libsql = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
-});
+let prismaClient: PrismaClient;
+
+if (process.env.TURSO_DATABASE_URL) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { PrismaLibSql } = require("@prisma/adapter-libsql");
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { createClient } = require("@libsql/client");
+  const libsql = createClient({
+    url: process.env.TURSO_DATABASE_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  });
+  prismaClient = new PrismaClient({
+    adapter: new PrismaLibSql(libsql),
+    log: ["error", "warn"],
+  });
+} else {
+  prismaClient = new PrismaClient({
+    log: ["error", "warn"],
+  });
+}
 
 export const db =
-  global.prisma ||
-  new PrismaClient({
-    adapter: new PrismaLibSql(libsql),
-    log: ["query"],
-  });
+  global.prisma || prismaClient;
 
 if (process.env.NODE_ENV !== "production") global.prisma = db;
